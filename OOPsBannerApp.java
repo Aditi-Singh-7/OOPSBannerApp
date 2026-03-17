4f488f9346efa78a0d760169a1c1446660fba648
Aditi-Singh-7 <aditisingh95061@gmail.com>
2026-03-17 19:07:33 +0530

    UC3:Print OOPS

--- OOPsBannerApp.java
+++ OOPsBannerApp.java
@@ -1,21 +1,27 @@
-/**
- * OOPS Banner App
- *
- * UC2: Render OOPS as Banner using '*' and spaces
- *
- * @author Abhiram
- * @version 2.0
- */
+
 public class OOPsBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("  *****     *****     ******     ***** ");
-        System.out.println(" *     *   *     *   *      *   *     ");
-        System.out.println(" *     *   *     *   *      *   *     ");
-        System.out.println(" *     *   *     *   ******      ***** ");
-        System.out.println(" *     *   *     *   *                * ");
-        System.out.println(" *     *   *     *   *                * ");
-        System.out.println("  *****     *****    *           ***** " );
+        System.out.println(String.join(" ",
+                " ** ", "  ** ", "  ** ", "  ** "));
+
+        System.out.println(String.join(" ",
+                "*     *", " *     *", " *     *", " *      "));
+
+        System.out.println(String.join(" ",
+                "*     *", " *     *", " *     *", " *      "));
+
+        System.out.println(String.join(" ",
+                "*     ", " *     *", " **  ", "  *** "));
+
+        System.out.println(String.join(" ",
+                "*     *", " *     *", " *      ", "       *"));
+
+        System.out.println(String.join(" ",
+                "*     *", " *     *", " *      ", "       *"));
+
+        System.out.println(String.join(" ",
+                " ** ", "  ** ", " *      ", "  *** "));
     }
 }
