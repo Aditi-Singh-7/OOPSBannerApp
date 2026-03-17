f4a2900ec0f161f3d4be26e4bcf51d495c76b4d6
Aditi-Singh-7 <aditisingh95061@gmail.com>
2026-03-17 18:32:05 +0530

    UC2 : Print OOPS

--- OOPsBannerApp.java
+++ OOPsBannerApp.java
@@ -1,5 +1,21 @@
+/**
+ * OOPS Banner App
+ *
+ * UC2: Render OOPS as Banner using '*' and spaces
+ *
+ * @author Abhiram
+ * @version 2.0
+ */
 public class OOPsBannerApp {
+
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println("  *****     *****     ******     ***** ");
+        System.out.println(" *     *   *     *   *      *   *     ");
+        System.out.println(" *     *   *     *   *      *   *     ");
+        System.out.println(" *     *   *     *   ******      ***** ");
+        System.out.println(" *     *   *     *   *                * ");
+        System.out.println(" *     *   *     *   *                * ");
+        System.out.println("  *****     *****    *           ***** " );
     }
 }
